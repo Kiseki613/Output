f1c717a3b54f40b1369db285c53c90c6213da192
Kiseki613 <HUAJ1_15@uni.worc.ac.uk>
2017-11-16 15:36:43 +0000

    0

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 $(document).on("pagecreate","#pageone",function(){
   $('#button1').on("click", function(){
-    random();
+    click();
   });            
 });            
 
 function random() {
-     return !Math.round(Math.random());
+   
+    return !Math.round(Math.random());
 }
 
+function click(){
+    
+    var rand = random();
+    
+    if(rand) {
+        
+        
+        navigator.notification.beep(1);
+ 
+    }
+    else {
+        
+       navigator.notification.beep(2);
+    }
+    
 }
